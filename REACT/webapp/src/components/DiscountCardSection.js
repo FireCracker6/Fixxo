c86a90338be2aa2e0fba1515bf107f878884c518
FireCracker6 <111967393+FireCracker6@users.noreply.github.com>
2022-10-24 18:04:16 +0200

    backup

--- REACT/webapp/src/components/DiscountCardSection.js
+++ REACT/webapp/src/components/DiscountCardSection.js
@@ -8,25 +8,45 @@ function DiscountCard2() {
     <section className="disc-card container">
    
 
+{/*      
+      <div className=" row row-cols-2 row-cols-sm-1  row-cols-md-2  row-cols-xl-3 g-4 "> */}
+        
+      <div className="grid-wrapper row row-cols-2 row-cols-sm-1  row-cols-md-2 row-cols-xl-3 g-4 ">
+      <div className="item-1">
+        <h4>Latest Product</h4>
+       <DiscountCardMobile />
+       <DiscountCardMobile />
+       <DiscountCardMobile />
+       
+       </div>
+         
+               
+   
+        
+       <div className="item-2">
+       <h4>Best Selling Product</h4>
+       <DiscountCardMobile />
+       <DiscountCardMobile />
+       <DiscountCardMobile />
+       
+       </div>
+               
+            
+                
      
-      <div className="row row-cols-2 row-cols-sm-2  row-cols-md-2  row-cols-xl-3 g-4 ">
         
  
+           
+       <div className="item-3">
+       <h4>Top Ratings</h4>
        <DiscountCardMobile />
        <DiscountCardMobile />
        <DiscountCardMobile />
-       <DiscountCardMobile />
-       <DiscountCardMobile />
-       <DiscountCardMobile />
-       <DiscountCardMobile />
-       <DiscountCardMobile />
-       <DiscountCardMobile />
-       <DiscountCardMobile />
-       <DiscountCardMobile />
-       <DiscountCardMobile />
-
-              
-            </div>
+       
+       </div>
+               
+               
+             </div>
           </section>  
               
 
